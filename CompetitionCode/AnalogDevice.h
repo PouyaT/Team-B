9bba49201c3299837ac2a75756f8f7b618c4868b
darbycobb <31716469+darbycobb@users.noreply.github.com>
2018-01-31 19:46:20 -0600

    analog yay

---
+++ CompetitionCode/AnalogDevice.h
@@ -0,0 +1,16 @@
+#ifndef ANALOGDEVICE_H
+#define ANALOGDEVICE_H
+
+
+class AnalogDevice
+{
+    public:
+        AnalogDevice();
+        virtual ~AnalogDevice();
+
+    protected:
+
+    private:
+};
+
+#endif // ANALOGDEVICE_H
